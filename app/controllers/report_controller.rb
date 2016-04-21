a528f91acc2d3849c9b616edb09fccc9e1044f18
Russell Osborne <russell@burningpony.com>
2016-04-21 10:44:12 -0400

    Inital Generators

---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,7 @@
+class ReportController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/jobs/create_account_report_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreateAccountReportJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
